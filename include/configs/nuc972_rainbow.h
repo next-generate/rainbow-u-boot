43962b7af930006bfb55a40dd4bbb9b3584be0a6
jiuzhuaxiong <jiuzhuaxiong@163.com>
2019-07-25 04:57:10 -0700

    reduce the system size to 26MB

--- include/configs/nuc972_rainbow.h
+++ include/configs/nuc972_rainbow.h
@@ -126,7 +126,7 @@
 #define CONFIG_SYS_NAND_ONFI_DETECTION 1
 
 #define MTDIDS_DEFAULT "nand0=nand0"
-#define MTDPARTS_DEFAULT "mtdparts=nand0:0x80000@0x0(u-boot-master),0x80000@0x80000(env-master),0x80000@0x100000(u-boot-backup),0x80000@0x180000(env-backup),0x400000@0x200000(kernel-master),0x3200000@0x600000(rootfs),0x3200000@0x3800000(app),-(data)"
+#define MTDPARTS_DEFAULT "mtdparts=nand0:0x80000@0x0(u-boot-master),0x80000@0x80000(env-master),0x80000@0x100000(u-boot-backup),0x80000@0x180000(env-backup),0x400000@0x200000(kernel-master),0x2600000@0x600000(rootfs),0x3200000@0x3800000(app),-(data)"
 #define MTD_ACTIVE_PART "nand0,7"
 
 /*#define CONFIG_CMD_NAND_YAFFS2 1 */
