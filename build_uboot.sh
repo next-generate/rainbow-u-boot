01c1a6b9f0f15b68c23ec69a8a6286fc248e4c91
jiuzhuaxiong <jiuzhuaxiong@163.com>
2019-08-25 10:33:44 +0800

    add fw_printenv tool build

--- build_uboot.sh
+++ build_uboot.sh
@@ -3,6 +3,7 @@
 chmod u+x scripts/*.sh
 make distclean
 make nuc972_rainbow_defconfig
-make  -j 32
+make  -j 16
+make env
 # file u-boot.bin and u-boot-spl.bin in the spl directory and source root directory
 
